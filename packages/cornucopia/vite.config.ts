4275a03b1b2104da4334485b865aa608c3ff4442
chenzhigang <979703986@qq.com>
2022-06-21 17:39:53 +0800

    subcode

--- packages/cornucopia/vite.config.ts
+++ packages/cornucopia/vite.config.ts
@@ -1,11 +1,39 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import DefineOptions from 'unplugin-vue-define-options/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
+
+const resolve = dir => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-      vue()
-  ]
+    vue(),
+    DefineOptions(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      resolvers: [
+        VantResolver()
+      ],
+    }),
+    Components({
+      resolvers: [
+        VantResolver(),
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      'src': resolve('src'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
 })

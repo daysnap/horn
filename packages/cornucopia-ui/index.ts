ccd889c5aaf1be3fec04821aba38738841bc3680
chenzhigang <979703986@qq.com>
2022-06-21 18:14:18 +0800

    subcode

---
+++ packages/cornucopia-ui/index.ts
@@ -0,0 +1,7 @@
+
+
+import _CpaPicker from './src/cpa-picker/index.vue'
+
+export const CpaPicker = _CpaPicker
+
+// export * as CpaPicker from './src/cpa-picker/index.vue'
---
+++ packages/cornucopia-ui/src/cpa-picker/index.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <button>picker</button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

af1c1cd6a7b485234ac664af7fcb9c1f31b4dfd4
王海波 <1056140533@qq.com>
2022-07-06 22:44:54 +0800

    chore: to志刚

--- packages/horn-ui/src/hor-keyboard/index.vue
+++ packages/horn-ui/src/hor-keyboard/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <div>
-      <span></span>
-      <button></button>
+  <van-popup class="hor-keyboard" position="bottom" :show="true">
+    <div class="hor-keyboard-header">
+      <span class="hor-keyboard-value"></span>
+      <button class="hor-keyboard-cancel whb">取消</button>
     </div>
-  </div>
+    <ul class="as-keyboard-content">
+      <li>123321</li>
+    </ul>
+  </van-popup>
 </template>
 
 <script setup lang="ts"></script>
@@ -12,5 +15,44 @@
 <style lang="scss" scoped>
   @import '../styles/define.scss';
   @include b(keyboard) {
+    &:deep {
+      background-color: rgb(155, 41, 41);
+    }
+    &-header {
+      @extend %df;
+      @extend %aic;
+      @extend %jcs;
+      @extend %bw05;
+      @extend %bwb;
+      padding: j(10) 0 j(10) j(10);
+    }
+    &-value {
+      @extend %df1;
+      @extend %df;
+      @extend %aic;
+      @extend %c3;
+      @extend %bsb;
+      padding: 0 j(16);
+      font-size: j(14);
+      background-color: #fff;
+      border-radius: j(8);
+      height: j(40);
+      &.place-value {
+        color: #cbcbcb;
+      }
+    }
+    &-cancel {
+      @extend %df;
+      @extend %aic;
+      @extend %jcc;
+      @extend %cp;
+      @extend %ao8;
+      border: none;
+      color: #0095d9;
+      height: j(40);
+      padding: 0 j(20);
+      font-size: j(16);
+      background-color: transparent;
+    }
   }
 </style>

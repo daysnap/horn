e306d461afae9209cdafe3bf5e467cb90c22c68e
王海波 <>
2022-07-06 18:21:55 +0800

    feat: hor-keyboard

--- packages/horn-ui/src/hor-keyboard/index.vue
+++ packages/horn-ui/src/hor-keyboard/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <div> </div>
+  <div>
+    <div>
+      <span></span>
+      <button></button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  @import '../styles/define.scss';
+  @include b(keyboard) {
+  }
+</style>

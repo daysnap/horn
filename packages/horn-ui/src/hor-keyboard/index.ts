63d535a9bb24c9899d5af63398895620c7740bf3
王海波 <>
2022-07-08 17:14:16 +0800

    feat: keyboard

--- packages/horn-ui/src/hor-keyboard/index.ts
+++ packages/horn-ui/src/hor-keyboard/index.ts
@@ -1,7 +1,9 @@
 import Component from './index.vue'
 import { withInstall } from '../utils'
+import { KeyBoard } from './function-call'
 export const HorKeyboard = withInstall<typeof Component>(Component)
 export type HorKeyboardInstance = InstanceType<typeof HorKeyboard>
+export { KeyBoard }
 declare module 'vue' {
   export interface GlobalComponents {
     HorKeyboard: typeof HorKeyboard
